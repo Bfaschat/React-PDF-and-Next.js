6cda217d3b0589b5de16c3b871f5ea94a36c8b29
bfaschat <bfaschatinc@gmail.com>
2024-08-19 16:16:45 +0100

    2 test

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,37 @@
-// pages/_app.js
-import { useEffect } from 'react';
-import { applyPromisePolyfill } from '@/utils/polyfills'; // Adjust the path as needed
-import '@/styles/globals.css';
+import "@/styles/globals.css";
+import { StrictMode, useEffect } from 'react';
+import { useRouter } from "next/router";
+
+export const applyPromisePolyfill = () => {
+  if (typeof Promise.withResolvers === "undefined") {
+    if (typeof window !== "undefined") {
+      window.Promise.withResolvers = function () {
+        let resolve, reject;
+        const promise = new Promise((res, rej) => {
+          resolve = res;
+          reject = rej;
+        });
+        return { promise, resolve, reject };
+      };
+    } else if (typeof global !== "undefined") {
+      global.Promise.withResolvers = function () {
+        let resolve, reject;
+        const promise = new Promise((res, rej) => {
+          resolve = res;
+          reject = rej;
+        });
+        return { promise, resolve, reject };
+      };
+    }
+  }
+};
+
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
   useEffect(() => {
-    applyPromisePolyfill();
-  }, []);
-  
+      applyPromisePolyfill();
+  }, []); 
   return <Component {...pageProps} />;
 }
