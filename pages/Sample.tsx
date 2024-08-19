da24eafbeff8dcb23a51c419e0082dd2176d447c
bfaschat <bfaschatinc@gmail.com>
2024-08-19 19:54:02 +0100

    I think this will work now

--- pages/Sample.tsx
+++ pages/Sample.tsx
@@ -6,10 +6,8 @@ import 'react-pdf/dist/esm/Page/TextLayer.css';
 
 import type { PDFDocumentProxy } from 'pdfjs-dist';
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
-  import.meta.url,
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.mjs`;
+
 
 const options = {
   cMapUrl: '/cmaps/',
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import './Sample.css';
 
 import type { AppProps } from 'next/app';
+import 'core-js/full/promise/with-resolvers.js';
+import { polyfillPromiseWithResolvers } from '../utils/PromiseWithResolvers';
+
+polyfillPromiseWithResolvers();
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
